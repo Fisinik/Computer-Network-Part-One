943a7dfeae9ec81bb8275d2168c08cbe51b4c891
blendagashi <blendagashi@live.com>
2017-04-18 14:25:55 +0200

    Update TCPClient.py

--- TCPClient/TCPClient.py
+++ TCPClient/TCPClient.py
@@ -1,1 +1,12 @@
 from socket import *
+
+host= "10.20.1.54"
+port = 9000
+
+sock= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+sock.connect((host,port))
+while True:
+    data = input("message: ")
+    sock.send(data.encode("utf-8")
+    print("response: ", sock.recv(1024).decode("utf-8"))
